b10f8b1db157ac2b442dcfbdf1add9735ec777ee
Rob Weber <robweberjr@gmail.com>
2012-12-03 14:34:04 -0600

    use storage server instead of reading/writing text file

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -1,8 +1,14 @@
 import xbmc
 import xbmcaddon
-
+try:
+    import StorageServer
+except:
+    import storageserverdummy as StorageServer
+    
 __addon_id__= 'service.libraryautoupdate'
 __Addon = xbmcaddon.Addon(__addon_id__)
+
+cache = StorageServer.StorageServer(__addon_id__,24)
 
 def data_dir():
     return __Addon.getAddonInfo('profile')
@@ -16,11 +22,17 @@ def log(message,loglevel=xbmc.LOGNOTICE):
 def showNotification(title,message):
     xbmc.executebuiltin("Notification(" + encode(title) + "," + encode(message) + ",4000," + xbmc.translatePath(__Addon.getAddonInfo('path') + "/resources/images/clock.png") + ")")
 
+def setSetting(name,value):
+    __Addon.setSetting(name,value)
+
+def setCache(name,value):
+    cache.set(name,value)
+
 def getSetting(name):
     return __Addon.getSetting(name)
 
-def setSetting(name,value):
-    __Addon.setSetting(name,value)
+def getCache(name):
+    return cache.get(name)
     
 def getString(string_id):
     return __Addon.getLocalizedString(string_id)
