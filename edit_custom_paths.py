85d2adc271fab6b6bf51f5ee26643b9a1da64704
Rob Weber <rweber@ecec.com>
2017-01-31 10:10:48 -0600

    put strings in strings.xml

--- edit_custom_paths.py
+++ edit_custom_paths.py
@@ -6,24 +6,14 @@ dialog = xbmcgui.Dialog()
 #show the disclaimer - do this every time
 dialog.ok(utils.getString(30031),"",utils.getString(30032),utils.getString(30033))
 
-#show list of current paths
-    #click to edit
-    #add new
-
-#select path to scan
-#choose default or custom timer
-#select time
-
-#save
-
 def selectPath():
     path = {'expression':'0 */2 * * *'}
     
     #select path to scan
-    path['path'] = dialog.browse(0,'Browse for path','video')
+    path['path'] = dialog.browse(0,utils.getString(30023),'video')
 
     #create expression
-    path['expression'] = dialog.input('Cron Expression',path['expression'])
+    path['expression'] = dialog.input(utils.getString(30056),path['expression'])
     
     return path
 
@@ -39,7 +29,7 @@ def showMainScreen():
             options.append(aPath['path'] + ' - ' + aPath['expression'])
             
         #show the gui
-        exitCondition = dialog.select("Edit Custom Paths",options)
+        exitCondition = dialog.select(utils.getString(30020),options)
         
         if(exitCondition >= 0):
             if(exitCondition == 0):
@@ -48,7 +38,7 @@ def showMainScreen():
                 customPaths.addPath(path)
             else:
                 #delete?
-                if(dialog.yesno(heading="Choose Action",line1="Delete this item?")):
+                if(dialog.yesno(heading=utils.getString(30021),line1=utils.getString(30022))):
                     #delete this path - subtract one because of "add" item
                     customPaths.deletePath(exitCondition -1)
             
